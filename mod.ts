c0b55ad080b82b001b479a524ee1a760a9b76a03
sergio-franca-wave <139240473+sergio-franca-wave@users.noreply.github.com>
2024-09-25 22:34:13 -0300

    .

--- mod.ts
+++ mod.ts
@@ -1,32 +1,5 @@
-import type { App, FnContext } from "@deco/deco";
-import { fetchSafe } from "apps/utils/fetch.ts";
-import { createHttpClient } from "apps/utils/http.ts";
+import manifest from "./manifest.gen.ts";
 import { PreviewContainer } from "apps/utils/preview.tsx";
-import type { Secret } from "apps/website/loaders/secret.ts";
-import manifest, { Manifest } from "./manifest.gen.ts";
-import { ClientInterfaceExample } from "./utils/client.ts";
-
-export type AppContext = FnContext<State, Manifest>;
-
-export interface Props {
-  /**
-   * @title Account Name
-   * @description erploja2 etc
-   */
-  account: string;
-
-  /**
-   * @title API token
-   * @description The token for accessing your API
-   */
-  token?: Secret;
-}
-
-// Here we define the state of the app
-// You choose what to put in the state
-export interface State extends Omit<Props, "token"> {
-  api: ReturnType<typeof createHttpClient<ClientInterfaceExample>>;
-}
 
 /**
  * @title Reviews App
@@ -34,24 +7,8 @@ export interface State extends Omit<Props, "token"> {
  * @category Tools
  * @logo https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1/0ac02239-61e6-4289-8a36-e78c0975bcc8
  */
-export default function App(props: Props): App<Manifest, State> {
-  const { token, account: _account } = props;
-
-  const _stringToken = typeof token === "string" ? token : token?.get?.() ?? "";
-
-  const api = createHttpClient<ClientInterfaceExample>({
-    base: `https://api.github.com/users/betovanw1`,
-    // headers: new Headers({ "Authorization": `Bearer ${stringToken}` }),
-    fetcher: fetchSafe,
-  });
-
-  // it is the state of the app, all data
-  // here will be available in the context of
-  // loaders, actions and workflows
-  const state = { ...props, api };
-
+export default function App() {
   return {
-    state,
     manifest,
   };
 }
