75de4e3dfd8088bed7e3bbc23489e0221780ee10
sergio-franca-wave <139240473+sergio-franca-wave@users.noreply.github.com>
2024-09-25 22:44:33 -0300

    .

--- mod.ts
+++ mod.ts
@@ -1,14 +1,23 @@
 import manifest from "./manifest.gen.ts";
 import { PreviewContainer } from "apps/utils/preview.tsx";
 
+export interface State {
+  name: string;
+}
+
 /**
  * @title Reviews App
  * @description This is the description of the app
  * @category Tools
  * @logo https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1/0ac02239-61e6-4289-8a36-e78c0975bcc8
  */
-export default function App() {
+export default function App(props: State) {
+  const state = {
+    ...props,
+  };
+
   return {
+    state,
     manifest,
   };
 }
